5f87c1ef596acdf1b49af57dbf0842c09c684406
manikandanNeo <bpo-manikandan.baskaran@neogrowth.in>
2021-03-10 12:03:56 +0530

    change clickables

--- src/screens/ListingScreen.js
+++ src/screens/ListingScreen.js
@@ -1,5 +1,12 @@
 import React, {useEffect, useState} from 'react';
-import {FlatList, Text, View, StyleSheet, Image} from 'react-native';
+import {
+  FlatList,
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
 import {connect} from 'react-redux';
 import {getListingsAction} from '../store/actions/listingAction';
 import Collapse from '../assets/images/collapse.png';
@@ -17,28 +24,24 @@ const Listing = ({listing, getListing}) => {
       <>
         {itm && (
           <>
-            <View style={styles.row}>
-              <Text
-                style={styles.titleStyle}
-                onPress={() => {
-                  setState({
-                    ...state,
-                    [itm.name]: state[itm.name] ? !state[itm.name] : true,
-                  });
-                }}>
-                {itm.name}
-              </Text>
+            <TouchableOpacity
+              onPress={() => {
+                setState({
+                  ...state,
+                  [itm.name]: state[itm.name] ? !state[itm.name] : true,
+                });
+              }}
+              style={styles.row}>
+              <Text style={styles.titleStyle}>{itm.name}</Text>
               <Image
                 style={styles.image}
                 source={state[itm.name] ? Collapse : Expand}
               />
-            </View>
+            </TouchableOpacity>
             {state[itm.name] && (
               <FlatList
                 ListEmptyComponent={() => (
-                  <Text style={styles.titleStyle}>
-                    No items in this category
-                  </Text>
+                  <Text style={styles.noItems}>No items in this category</Text>
                 )}
                 data={itm.sub}
                 keyExtractor={(it) => 'key' + it.id}
@@ -56,28 +59,24 @@ const Listing = ({listing, getListing}) => {
       <>
         {itm && (
           <>
-            <View style={styles.row}>
-              <Text
-                style={styles.titleStyle}
-                onPress={() => {
-                  setState({
-                    ...state,
-                    [itm.name]: state[itm.name] ? !state[itm.name] : true,
-                  });
-                }}>
-                {itm.name}
-              </Text>
+            <TouchableOpacity
+              onPress={() => {
+                setState({
+                  ...state,
+                  [itm.name]: state[itm.name] ? !state[itm.name] : true,
+                });
+              }}
+              style={styles.row}>
+              <Text style={styles.titleStyle}>{itm.name}</Text>
               <Image
                 style={styles.image}
                 source={state[itm.name] ? Collapse : Expand}
               />
-            </View>
+            </TouchableOpacity>
             {state[itm.name] && (
               <FlatList
                 ListEmptyComponent={() => (
-                  <Text style={styles.titleStyle}>
-                    No items in this category
-                  </Text>
+                  <Text style={styles.noItems}>No items in this category</Text>
                 )}
                 data={itm.sub}
                 keyExtractor={(it) => 'key' + it.id}
@@ -109,6 +108,7 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
     marginStart: 12,
+    marginTop: 12,
   },
   row: {
     flex: 1,
@@ -120,6 +120,11 @@ const styles = StyleSheet.create({
   image: {
     width: 20,
     aspectRatio: 1,
+  },
+  noItems: {
+    marginTop: 12,
+    fontSize: 18,
+    marginStart: 12,
   },
 });
 
